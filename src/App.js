85aba53a978f2216e979f2eb9e62afba849b86e4
Allex Valério Costa <allex.developer@gmail.com>
2020-08-28 07:08:10 -0300

    Desafio2, criado o metodo de adicao e remocao do repositorio

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
-function App() {
+export default function App() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepository(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+      title: `new repository ${Date.now()}`,
+      owner: "Allex Valério Costa"
+    });
+
+    const repository = response.data;
+
+    setRepository([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepository([...repositories.filter(
+      repository => repository.id !== id
+    )]);    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository =>
+        <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
-
-export default App;
